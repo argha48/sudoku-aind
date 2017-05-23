c04a25bd1a09e15f23120b690a0e188ccdf125ea
Argha Mondal <argha48@gmail.com>
2017-05-22 23:52:48 -0400

    Final Sudoku solver

--- solution.py
+++ solution.py
@@ -42,25 +42,30 @@ def naked_twins(values):
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
-
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
-    twins = [box for box in values.keys() if len(values[box]) == 2]
-    naked_twins = [[box1,box2] for box1 in twins \
-                   for box2 in peers[box1] \
-                   if set(values[box1]) == set(values[box2]) ]
-    for i in range(len(naked_twins)):
-        box1 = naked_twins[i][0]
-        box2 = naked_twins[i][1]
-        allpeers = set(peers[box1]) & set(peers[box2])
-        for apeer in allpeers:
-            if len(set(values[apeer])) > 2:
-                for num in values[box1]:
-                    values = assign_value(values, apeer, values[apeer].replace(num,''))
+    no_more_twins = False
+    while no_more_twins == False:
+        board_before = values.copy()
+        twins = [box for box in values.keys() if len(values[box]) == 2]
+        naked_twins = [[box1,box2] for box1 in twins \
+                       for box2 in peers[box1] \
+                       if set(values[box1]) == set(values[box2]) ]
+        for i in range(len(naked_twins)):
+            box1 = naked_twins[i][0]
+            box2 = naked_twins[i][1]
+            allpeers = set(peers[box1]) & set(peers[box2])
+            for apeer in allpeers:
+                if len(set(values[apeer])) > 1:
+                    for num in values[box1]:
+                        values = assign_value(values, apeer, \
+                                              values[apeer].replace(num,''))
+        if board_before == values:
+            no_more_twins = True
     return values
     
 
@@ -166,6 +171,14 @@ def solve(grid):
     return values
 
 if __name__ == '__main__':
+
+    """
+    values = {"G7": "2345678", "G6": "1236789", "G5": "23456789", "G4": "345678", "G3": "1234569", "G2": "12345678", "G1": "23456789", "G9": "24578", "G8": "345678", "C9": "124578", "C8": "3456789", "C3": "1234569", "C2": "1234568", "C1": "2345689", "C7": "2345678", "C6": "236789", "C5": "23456789", "C4": "345678", "E5": "678", "E4": "2", "F1": "1", "F2": "24", "F3": "24", "F4": "9", "F5": "37", "F6": "37", "F7": "58", "F8": "58", "F9": "6", "B4": "345678", "B5": "23456789", "B6": "236789", "B7": "2345678", "B1": "2345689", "B2": "1234568", "B3": "1234569", "B8": "3456789", "B9": "124578", "I9": "9", "I8": "345678", "I1": "2345678", "I3": "23456", "I2": "2345678", "I5": "2345678", "I4": "345678", "I7": "1", "I6": "23678", "A1": "2345689", "A3": "7", "A2": "234568", "E9": "3", "A4": "34568", "A7": "234568", "A6": "23689", "A9": "2458", "A8": "345689", "E7": "9", "E6": "4", "E1": "567", "E3": "56", "E2": "567", "E8": "1", "A5": "1", "H8": "345678", "H9": "24578", "H2": "12345678", "H3": "1234569", "H1": "23456789", "H6": "1236789", "H7": "2345678", "H4": "345678", "H5": "23456789", "D8": "2", "D9": "47", "D6": "5", "D7": "47", "D4": "1", "D5": "36", "D2": "9", "D3": "8", "D1": "36"}
+    display(values)
+    print('\n\n\n\n')
+    display(naked_twins(values))
+    
+    """
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
@@ -177,3 +190,4 @@ if __name__ == '__main__':
         pass
     except:
         print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+    
